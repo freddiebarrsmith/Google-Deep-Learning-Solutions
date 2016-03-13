8921cde10b9a8911434613baba3859506ced551d
root <fredbarrsmith@gmail.com>
2016-03-13 13:26:46 +0000

    calculator

---
+++ big.py
@@ -0,0 +1,11 @@
+import math
+
+a = pow(10, 9)
+b = pow(10, -6)
+million = pow(10, 6)
+for x in range(0, million):
+	a = a + b
+billion = pow(10, 9)
+z = a - billion
+print z
+# it equals 0.953674316406
